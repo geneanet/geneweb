e9b9ee8762272463d3e0ed0867399d7e1bfe414c
Julien Sagot <julien.sagot@geneanet.org>
2019-12-09 15:27:48 +0100

    Replace Util.capitale by Utf8.capitalize

--- lib/hutil.ml
+++ lib/hutil.ml
@@ -1,4 +1,3 @@
-(* $Id: hutil.ml,v 5.11 2007-09-12 09:58:44 ddr Exp $ *)
 (* Copyright (c) 2007 INRIA *)
 
 open Config
@@ -13,7 +12,7 @@ let commd_no_params conf =
 
 let link_to_referer conf =
   let referer = Util.get_referer conf in
-  let back = Util.capitale (Util.transl conf "back") in
+  let back = Utf8.capitalize (Util.transl conf "back") in
   if referer <> "" then
     Printf.sprintf "<a href=\"%s\">\
          <span class=\"fa fa-arrow-left fa-lg\" title=\"%s\"></span>\
@@ -124,7 +123,7 @@ let trailer = gen_trailer true
 
 let incorrect_request conf =
   let title _ =
-    Wserver.printf "%s" (Util.capitale (Util.transl conf "incorrect request"))
+    Wserver.printf "%s" (Utf8.capitalize (Util.transl conf "incorrect request"))
   in
   Wserver.http Wserver.Bad_Request;
   header conf title;
